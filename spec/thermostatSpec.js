0ac10354c743770a5cd4a15076e26d85c0143fa6
Student <student@Makerss-MacBook-Air.local>
2019-12-11 16:15:36 +0000

    Implement specifications 4 to 8

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -24,4 +24,30 @@ describe ("Thermostat", function() {
     }
     expect(function() {thermostat.down()}).toThrowError('You cannot go below 10 degrees')
   })
+
+  it ("If the power saving mode is on, the maximum temperature is 25", function() {
+    thermostat.savingModeOn();
+    for (var i = 0; i < 5; i++) {
+      thermostat.up()
+    }
+    expect(function() { thermostat.up() }).toThrowError('You cannot go above 25 degrees when power saving mode is on')
+  })
+
+  it ("If the power saving mode is off, the maximum temperature is 32", function() {
+    thermostat.savingModeOff();
+    for (var i = 0; i < 12; i++) {
+      thermostat.up()
+    }
+    console.log(thermostat.temperature())
+    expect(function() { thermostat.up() }).toThrowError('You cannot go above 32 degrees when power saving mode is off')
+  })
+
+  it ("should have power saving mode on by default", function() {
+    expect(thermostat._isPowerSavingModeOn).toEqual(true)
+  })
+
+  it ("should be able to reset the temperature to 20 degrees", function() {
+    thermostat.reset()
+    expect(thermostat.temperature()).toEqual(20)
+  })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,7 @@
 function Thermostat() {
   this._temperature = 20
+  this._isPowerSavingModeOn = true
+  this._MINIMUM_TEMPERATURE = 10
 }
 
 Thermostat.prototype.temperature = function() {
@@ -7,12 +9,29 @@ Thermostat.prototype.temperature = function() {
 }
 
 Thermostat.prototype.up = function() {
+  if (this._isPowerSavingModeOn && this._temperature === 25) {
+    throw new Error('You cannot go above 25 degrees when power saving mode is on')
+  } else if (!this._isPowerSavingModeOn && this._temperature === 32) {
+    throw new Error('You cannot go above 32 degrees when power saving mode is off')
+  }
   return this._temperature +=1
 }
 
 Thermostat.prototype.down = function() {
-  if (this._temperature === 10) {
+  if (this._temperature === this._MINIMUM_TEMPERATURE) {
     throw new Error('You cannot go below 10 degrees')
   }
   this._temperature -= 1
+}
+
+Thermostat.prototype.savingModeOn = function() {
+  this._isPowerSavingModeOn = true
+}
+
+Thermostat.prototype.savingModeOff = function() {
+  this._isPowerSavingModeOn = false
+}
+
+Thermostat.prototype.reset = function() {
+  this._temperature = 20
 }
