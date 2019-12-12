e6e08f237dee832ae1d257807087138d837d7355
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2019-12-12 16:20:52 +0000

    Add APIs

--- src/interface.js
+++ src/interface.js
@@ -33,4 +33,22 @@ $(document).ready(function() {
     thermostat.switchPowerSavingModeOff();
     $("#power-saving-status").text("off");
   });
+
+  $("#change-city").click(function(event) {
+    var cityName = document.getElementById("city-name").value;
+    getWeather(cityName);
+  });
+
+  function getWeather(cityName) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=";
+    var appId = "&APPID=7dca333c2a0290c11d8c820868e8f829";
+    $.getJSON(url + cityName + appId, function(data) {
+      $('#weather-city').text(data.name);
+      $('#weather-temperature').text(Math.round(data.main.temp - 273.15));
+      $('#weather-description').text(data.weather[0].description);
+      // $('#weather-temperature').text(JSON.stringify(data));
+    });
+  };
+
+  getWeather("London");
 });
